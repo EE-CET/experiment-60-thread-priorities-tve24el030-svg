3bda6ca4d11e518e351d4e0accdf120cf96a6c2d
tve24el030-svg <tve24el030@cet.ac.in>
2026-02-20 09:56:46 +0530

    code uploaded

--- src/ThreadPriorityDemo.java
+++ src/ThreadPriorityDemo.java
@@ -1,21 +1,23 @@
 class PriorityThread extends Thread {
     public void run() {
-        // Empty run method, we just need the thread object to exist and have properties
+    
     }
 }
 
 public class ThreadPriorityDemo {
     public static void main(String[] args) {
         // TODO: Create three threads (t1, t2, t3)
-        
+        PriorityThread t1=new PriorityThread();
+        PriorityThread t2=new PriorityThread();
+        PriorityThread t3=new PriorityThread();
         // TODO: Set priorities
-        // t1.setPriority(Thread.MIN_PRIORITY);
-        // t2.setPriority(Thread.NORM_PRIORITY);
-        // t3.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.NORM_PRIORITY);
+        t3.setPriority(Thread.MAX_PRIORITY);
         
         // TODO: Print priorities in the required format
-        // System.out.println("Thread 1 Priority: " + t1.getPriority());
-        // System.out.println("Thread 2 Priority: " + t2.getPriority());
-        // System.out.println("Thread 3 Priority: " + t3.getPriority());
+        System.out.println("Thread 1 Priority: " + t1.getPriority());
+        System.out.println("Thread 2 Priority: " + t2.getPriority());
+        System.out.println("Thread 3 Priority: " + t3.getPriority());
     }
 }
